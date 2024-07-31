5689f382e00c177a4c8b76f534d1c054097f8e1f
daanr <daanrsantiago@gmail.com>
2024-07-31 07:06:36 -0300

    start to develop python main function

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,7 +1,22 @@
 import requests
 
-url = "http://localhost:8080/optimizationRun/1/chromosome/notEvaluated"
+base_url = "http://localhost:8080"
 
-response = requests.request("GET", url, data=payload)
+optimization_run_id = 1
 
-print(response.text)
+
+
+def get_not_evaluated_chromosome(optimization_run_id: int)->object:
+    get_chormossome_path = f"/optimizationRun/{optimization_run_id}/chromosome/notEvaluated"
+    response = requests.request("GET", base_url + get_chormossome_path)
+
+def post_evaluation_result(chromosome_id: int, evaluation_id: str, fitness: float)-> object:
+    post_response_path = f"/chromosome/{chromosome_id}/evaluationResult"
+    payload = {
+        "fitness": fitness,
+        "evaluationId": evaluation_id
+    }
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("POST", base_url + post_response_path, json=payload, headers=headers)
+    return response.json
+
